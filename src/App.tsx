971b2c97d72f7455980b70acb0abc05dfb67c08f
tong.yao <tong.yao@check24.de>
2023-03-17 09:54:07 +0100

    loading svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './styles.css';
+import IMAGE from './react.png';
+import LOGO from './react.svg';
 
 export const App = () => {
   return (
-    <div>
-      <h1>React App starter Template</h1>
-    </div>
+    <>
+      <div>
+        <h1>React App starter Template</h1>
+        <img src={IMAGE} alt='react logo' width='300' height='300' />
+        <img src={LOGO} alt='react logo' width='300' />
+      </div>
+    </>
   );
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -18,6 +18,14 @@ const config = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ],
   },
   output: {
